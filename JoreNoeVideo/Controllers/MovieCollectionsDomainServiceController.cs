88fc3d9861b5dfb230ef04dd68d7433c871c4680
Dsirs1 <2199872549@qq.com>
2021-03-27 22:12:38 +0800

    Update MovieCollectionsDomainServiceController.cs

--- JoreNoeVideo/Controllers/MovieCollectionsDomainServiceController.cs
+++ JoreNoeVideo/Controllers/MovieCollectionsDomainServiceController.cs
@@ -1,4 +1,6 @@
-﻿using JoreNoeVideo.DomainServices;
+﻿using JoreNoeVideo.CommonInterFaces;
+using JoreNoeVideo.Domain.Models;
+using JoreNoeVideo.DomainServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -16,6 +18,34 @@ namespace JoreNoeVideo.API.Controllers
         public MovieCollectionsDomainServiceController(IMovieCollectionsDomainService MovieCollectionsDomainService)
         {
             this.MovieCollectionsDomainService = MovieCollectionsDomainService;
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost("AddMovieCollectionsDomain")]
+        public async Task<ActionResult<APIReturnInFo<MovieCollections>>> AddMovieCollectionsDomain(MovieCollections model)
+        {
+            return APIReturnInFo<MovieCollections>.Success(await this.MovieCollectionsDomainService.AddMovieCollections(model));
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpDelete("{Id}/RemovedMovieCollectionsDomain")]
+        public async Task<ActionResult<APIReturnInFo<MovieCollections>>> RemovedMovieCollectionsDomain(Guid Id)
+        {
+            return APIReturnInFo<MovieCollections>.Success(await this.MovieCollectionsDomainService.RemovedMovieCollections(Id));
+        }
+
+        [HttpPut("EditMovieCollectionsDomain")]
+        public async Task<ActionResult<APIReturnInFo<MovieCollections>>> EditMovieCollectionsDomain(MovieCollections model)
+        {
+            return APIReturnInFo<MovieCollections>.Success(await this.MovieCollectionsDomainService.EditMovieCollections(model));
         }
     }
 }
