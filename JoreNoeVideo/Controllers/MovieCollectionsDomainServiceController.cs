4b47c13bd6495fad905730c37a2b32d5e48792b8
Dsirs1 <72720244+Dsirs1@users.noreply.github.com>
2021-03-27 21:39:58 +0800

    1

---
+++ JoreNoeVideo/Controllers/MovieCollectionsDomainServiceController.cs
@@ -0,0 +1,21 @@
+﻿using JoreNoeVideo.DomainServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoreNoeVideo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieCollectionsDomainServiceController : ControllerBase
+    {
+        private readonly IMovieCollectionsDomainService MovieCollectionsDomainService;
+        public MovieCollectionsDomainServiceController(IMovieCollectionsDomainService MovieCollectionsDomainService)
+        {
+            this.MovieCollectionsDomainService = MovieCollectionsDomainService;
+        }
+    }
+}
