a174bd063571c67b4c9230149223b1ee635c5db7
JoreNoe <57209261+JoraNoe@users.noreply.github.com>
2021-03-28 15:31:00 +0800

    添加用户观看历史

---
+++ JoreNoeVideo.Domain/Models/UserWatchHistory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoreNoeVideo.Domain.Models
+{
+    /// <summary>
+    /// 用户观看历史
+    /// </summary>
+    public class UserWatchHistory
+    {
+        /// <summary>
+        /// 电影名称
+        /// </summary>
+        public string MovieName { get; set; }
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        public Guid UserId { get; set; }
+        /// <summary>
+        /// 影片Id
+        /// </summary>
+        public Guid MovieId { get; set; }
+        /// <summary>
+        /// 影片链接
+        /// </summary>
+        public string MovieLink { get; set; }
+    }
+}
