676405c91625b2379530083bf729da8f39593bb9
JoreNoe <57209261+JoraNoe@users.noreply.github.com>
2021-03-29 21:29:02 +0800

    添加程序

---
+++ JoreNoeVideo.Domain/Models/KoreanDramaOpera.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoreNoeVideo.Domain.Models
+{
+    /// <summary>
+    /// 韩剧
+    /// </summary>
+    public class KoreanDrama
+    {
+        /// <summary>
+        /// 影片名称
+        /// </summary>
+        public string MovieName { get; set; }
+        /// <summary>
+        /// 影片图片地址
+        /// </summary>
+        public string MovieImgUrl { get; set; }
+        /// <summary>
+        /// 链接
+        /// </summary>
+        public string MovieLink { get; set; }
+        /// <summary>
+        /// 详情
+        /// </summary>
+        public string MovieDesc { get; set; }
+
+        /// <summary>
+        /// 底部标题
+        /// </summary>
+        public string MovieTitle { get; set; }
+    }
+}
---
+++ JoreNoeVideo.Domain/Models/UserOptionHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoreNoeVideo.Domain.Models
+{
+    /// <summary>
+    /// 用户进入历史记录
+    /// </summary>
+    public class UserOptionHistory:BaseModel
+    {
+        /// <summary>
+        /// 用户Ip地址
+        /// </summary>
+        public string  UserIpAddress { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string  Remark { get; set; }
+    }
+}
